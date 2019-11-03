36b306d379fcf752309f161f9429b84ed7963199
Klaudia <klaudia.krzywda@gmail.com>
2019-11-03 18:24:51 +0000

    Updates

---
+++ precache-manifest.c4d92c2bccb7436aa839977dcea13e58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82cc03ff660a783387976d52a25fb7bd",
+    "url": "/tivix-dashboard/index.html"
+  },
+  {
+    "revision": "6d43f0d43f4345dcc093",
+    "url": "/tivix-dashboard/static/css/main.628ff47d.chunk.css"
+  },
+  {
+    "revision": "2d809368d649f62bb814",
+    "url": "/tivix-dashboard/static/js/2.b4e1510b.chunk.js"
+  },
+  {
+    "revision": "6d43f0d43f4345dcc093",
+    "url": "/tivix-dashboard/static/js/main.7b19d0ac.chunk.js"
+  },
+  {
+    "revision": "1a6c965796bddfda424c",
+    "url": "/tivix-dashboard/static/js/runtime-main.680ddb19.js"
+  },
+  {
+    "revision": "6f009fcfb536e61d890ba97480780455",
+    "url": "/tivix-dashboard/static/media/person1.6f009fcf.jpeg"
+  }
+]);
